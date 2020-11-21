a9422ae1f9a6b9ecccececd1f0f16b7a93847375
Ralph Joachim <ralph.joachim@docusign.com>
2020-11-21 05:25:58 -0800

    working https

--- src/main/java/com/itembase/currency/CurrencyController.java
+++ src/main/java/com/itembase/currency/CurrencyController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -44,7 +41,7 @@ public class CurrencyController {
     @Autowired
     private CurrencyService currencyService;
 
-    @PostMapping("/status")
+    @GetMapping("/status")
     public ResponseEntity<String> status() {
         return ResponseEntity.ok("Server up");
     }
